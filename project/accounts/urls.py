87f1d0154bde0e6fc9d4ac4d3976951fce658ba6
ago1256 <ago1256@yandex.ru>
2025-05-01 22:02:39 +0300

    3

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -4,4 +4,4 @@ from .views import registration_step1, registration_step2
 urlpatterns = [
     path('registration/', registration_step1, name='registration_step1'),
     path('registration/details/', registration_step2, name='registration_step2'),
-]
+] 
